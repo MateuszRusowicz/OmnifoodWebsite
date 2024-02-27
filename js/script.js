a8ed99967e693dc9275ac5e73d868647c56b529f
MateuszRusowicz <130058955+MateuszRusowicz@users.noreply.github.com>
2024-02-27 14:25:50 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// setting current year
+const yearEl = document.querySelector(".footer-year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+// show mobile nav
+const navBtn = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+navBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
+
+// Smooth Scrolling
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+// STICKY NAV
+
+const heroEl = document.querySelector(".hero-section");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(heroEl);
